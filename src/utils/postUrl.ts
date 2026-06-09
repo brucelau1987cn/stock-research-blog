40a16336a12bd14a5a844d1ef829f22fe9e2431b
brucelau1987cn <brucelau1987cn@users.noreply.github.com>
2026-06-09 19:52:23 +0800

    fix: use monthly serenity short links

---
+++ src/utils/postUrl.ts
@@ -0,0 +1,8 @@
+import type { CollectionEntry } from 'astro:content';
+
+export function getPostUrl(post: CollectionEntry<'blog'>) {
+	if (post.data.tags?.includes('白发股神') && /^\d{6}$/.test(post.id)) {
+		return `/${post.id}/`;
+	}
+	return `/blog/${post.id}/`;
+}
