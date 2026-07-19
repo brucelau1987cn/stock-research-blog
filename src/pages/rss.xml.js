18bbce31353b6fd161a549dcf8157d7951027571
brucelau1987cn <brucelau1987cn@users.noreply.github.com>
2026-07-19 08:40:13 +0800

    fix: harden site metadata and accessibility

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,7 +4,9 @@ import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 import { getPostUrl } from '../utils/postUrl';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const posts = (await getCollection('blog'))
+		.filter((post) => !post.data.serenityArchive)
+		.sort((a, b) => (b.data.updatedDate ?? b.data.pubDate).valueOf() - (a.data.updatedDate ?? a.data.pubDate).valueOf());
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
